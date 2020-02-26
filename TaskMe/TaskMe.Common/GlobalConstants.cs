a8a5a5626f1418e846da11a4d4425e7b0e0e6515
alexzagorv <alel_zagorov@abv.bg>
2020-02-26 22:15:52 +0200

    update Home view

--- TaskMe/TaskMe.Common/GlobalConstants.cs
+++ TaskMe/TaskMe.Common/GlobalConstants.cs
@@ -5,5 +5,9 @@
         public const string SystemName = "TaskMe";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string ManagerRoleName = "Manager";
+
+        public const string SupervisorRoleName = "Supervisor";
     }
 }
