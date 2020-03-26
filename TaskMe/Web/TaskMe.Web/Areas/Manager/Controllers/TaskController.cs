c6f31489d1b047385831006e55bc33205158ee58
alexzagorv <alel_zagorov@abv.bg>
2020-03-26 16:38:19 +0200

    Add Task controller and view. Other view fixes

---
+++ TaskMe/Web/TaskMe.Web/Areas/Manager/Controllers/TaskController.cs
@@ -0,0 +1,23 @@
+﻿namespace TaskMe.Web.Areas.Manager.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class TaskController : ManagerController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(int id)
+        {
+            return this.RedirectToAction("Index", "Task");
+        }
+    }
+}
