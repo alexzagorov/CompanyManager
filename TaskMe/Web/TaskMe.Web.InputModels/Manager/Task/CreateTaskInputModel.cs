912aa6ff230bd165f00100f59abd065fe0343888
alexzagorv <alel_zagorov@abv.bg>
2020-03-27 16:43:46 +0200

    Views fixes. Add popper.js and bootstrap-select

---
+++ TaskMe/Web/TaskMe.Web.InputModels/Manager/Task/CreateTaskInputModel.cs
@@ -0,0 +1,35 @@
+﻿namespace TaskMe.Web.InputModels.Manager.Task
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using TaskMe.Common;
+
+    public class CreateTaskInputModel
+    {
+        public CreateTaskInputModel()
+        {
+            this.Participants = new HashSet<int>();
+        }
+
+        [Required]
+        [StringLength(DataValidationConstants.TaskNameLength)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(DataValidationConstants.TaskDescriptionLength)]
+        public string Description { get; set; }
+
+        [Required]
+        [DisplayName("Start Date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DisplayName("End Date")]
+        [DataType(DataType.Date)]
+        public DateTime? EndDate { get; set; }
+
+        public ICollection<int> Participants { get; set; }
+    }
+}
